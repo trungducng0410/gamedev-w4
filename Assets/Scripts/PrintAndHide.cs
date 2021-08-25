180667aa445720df2c062ab2c284e7ecb38aea9e
jamesnguyenseta <88813063+jamesnguyenseta@users.noreply.github.com>
2021-08-25 21:45:11 +0700

    merge 100 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
+    private int a;
     public Renderer rend;
+
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        a = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log($"{gameObject.name}:{++i}");
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.tag == "Blue" && i == a)
+        {
+            rend.enabled = false;
+        }
     }
 }
